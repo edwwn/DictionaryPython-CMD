2f6884ec7382f53de5c53e1ef504543dae4d395d
edwwn <40034470+edwwn@users.noreply.github.com>
2019-07-12 15:37:18 +0300

    End of Dictionary

--- dic.py
+++ dic.py
@@ -1,10 +1,28 @@
 import json
+from difflib import get_close_matches
 
 data =json.load(open("data.json"))
 
-def translate(word):
-    return data[word]
+def translate(w):
+    w=w.lower()
+    if w in data:
+         return data[w]
+    elif len(get_close_matches(w, data.keys())) >0:
+        yn = input("Did you Mean %s instead? Enter Y for Yes, or N for No:  " % get_close_matches(w, data.keys())[0]).upper()
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The Word %s Doesnt Exist, Please Double Check it" % w
+        else:
+            return "We Didn't Understand your Entry"
+    else:
+        return "The Word  %s Doesnt Exists  Double Check It." % w
 
 word = input ("Enter Word: ")
+output=translate(word)
 
-print (translate(word))
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+     print(output)
